ca2d0028d675dde7162532c22a1688d2e23de914
Your Name <you@example.com>
2023-10-03 23:59:58 +0900

    login

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 import { Inter } from 'next/font/google'
 import { Dashboard } from "../features/Dashboard/Dashboard";
 import { NextPage } from "next";
-import { ProtectedRoute } from '@/components/Auth/ProtectedRoute';
 
 const inter = Inter({ subsets: ['latin'] })
 
