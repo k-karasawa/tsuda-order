5fd5c2f1c167818c01d341c9da1818f588ef992a
Your Name <you@example.com>
2023-10-06 06:02:58 +0900

    tag-color-fix

---
+++ src/components/OrderList/ColorfulTagcomponent.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Tag } from 'antd';
+
+const TAG_COLORS: { [key: string]: string } = {
+  '作業中': 'blue',
+  'OH作業中': 'blue',
+  '受注済': 'blue',
+  '見積提出': 'blue',
+  '検証中': 'purple',
+  '失注': '#f5f5f5',
+  '完了': 'green',
+  '仮見積': 'orange',
+};
+
+interface ColorfulTagProps {
+  text: string;
+}
+
+export const ColorfulTag: React.FC<ColorfulTagProps> = ({ text }) => {
+  const color = TAG_COLORS[text] || 'gray';
+  return <Tag color={color}>{text}</Tag>;
+}
