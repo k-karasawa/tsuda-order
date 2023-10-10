6244fc1ccf9d5bc9a5573c14481e94c3592da475
Your Name <you@example.com>
2023-10-10 13:21:23 +0900

    refactoring

--- src/features/OrderEditDrawer/OrderEditDrawer.tsx
+++ src/features/OrderEditDrawer/OrderEditDrawer.tsx
@@ -5,6 +5,8 @@ import type { OrderListDataType } from '@/types/types';
 import dayjs from 'dayjs';
 import customParseFormat from 'dayjs/plugin/customParseFormat';
 import { useOrderUpdater } from './hooks/useOrderUpdater';
+import { CustomDatePicker } from './CustomDatePicker';
+import { useOrderEffect } from './hooks/useOrderEffect';
 
 dayjs.extend(customParseFormat);
 const { TextArea } = Input;
@@ -31,23 +33,7 @@ export const OrderEditDrawer: React.FC<OrderEditDrawerProps> = ({ children, sele
     setIsAttention((prevState: boolean) => !prevState);
   };
 
-useEffect(() => {
-    setIsAttention(!!selectedOrder?.attention);
-    if (selectedOrder) {
-      setDates({
-        estimate_date: selectedOrder.estimate_date || null,
-        order_date: selectedOrder.order_date || null,
-        desired_delivery_date: selectedOrder.desired_delivery_date || null,
-        shipment_date: selectedOrder.shipment_date || null,
-        item_receive_date: selectedOrder.item_receive_date || null,
-        item_return_date: selectedOrder.item_return_date || null,
-        send_document_date: selectedOrder.send_document_date || null,
-        receive_document_date: selectedOrder.receive_document_date || null,
-        accept_date: selectedOrder.accept_date || null,
-      });
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [selectedOrder]);
+  useOrderEffect(selectedOrder, setIsAttention, setDates);
 
   return (
     <>
@@ -170,79 +156,63 @@ useEffect(() => {
 
           <Row gutter={16}>
             <Col span={6}>
-              <Form.Item label="見積日">
-                <DatePicker
-                  value={dates.estimate_date ? dayjs(dates.estimate_date) : undefined}
-                  style={{ width: '100%' }}
-                  onChange={(date) => setDates(prev => ({ ...prev, estimate_date: date ? date.format("YYYY-MM-DD") : null }))}
-                />
-              </Form.Item>
+              <CustomDatePicker
+                label="見積日"
+                value={dates.estimate_date}
+                onChange={(value) => setDates(prev => ({ ...prev, estimate_date: value }))}
+              />
             </Col>
             <Col span={6}>
-              <Form.Item label="受注日">
-                <DatePicker
-                  value={dates.order_date ? dayjs(dates.order_date) : undefined}
-                  style={{ width: '100%' }}
-                  onChange={(date) => setDates(prev => ({ ...prev, order_date: date ? date.format("YYYY-MM-DD") : null }))}
-                />
-              </Form.Item>
+              <CustomDatePicker
+                label="受注日"
+                value={dates.order_date}
+                onChange={(value) => setDates(prev => ({ ...prev, order_date: value }))}
+              />
             </Col>
             <Col span={6}>
-              <Form.Item label="希望納期">
-                <DatePicker
-                  value={dates.desired_delivery_date ? dayjs(dates.desired_delivery_date) : undefined}
-                  style={{ width: '100%' }}
-                  onChange={(date) => setDates(prev => ({ ...prev, desired_delivery_date: date ? date.format("YYYY-MM-DD") : null }))}
-                />
-              </Form.Item>
+              <CustomDatePicker
+                label="希望納期"
+                value={dates.desired_delivery_date}
+                onChange={(value) => setDates(prev => ({ ...prev, desired_delivery_date: value }))}
+              />
             </Col>
             <Col span={6}>
-              <Form.Item label="出荷日">
-                <DatePicker
-                  value={dates.shipment_date ? dayjs(dates.shipment_date) : undefined}
-                  style={{ width: '100%' }}
-                  onChange={(date) => setDates(prev => ({ ...prev, shipment_date: date ? date.format("YYYY-MM-DD") : null }))}
-                />
-              </Form.Item>
+              <CustomDatePicker
+                label="出荷日"
+                value={dates.shipment_date}
+                onChange={(value) => setDates(prev => ({ ...prev, shipment_date: value }))}
+              />
             </Col>
           </Row>
 
           <Row gutter={16}>
             <Col span={6}>
-              <Form.Item label="現品受領日">
-                <DatePicker
-                  value={dates.item_receive_date ? dayjs(dates.item_receive_date) : undefined}
-                  style={{ width: '100%' }}
-                  onChange={(date) => setDates(prev => ({ ...prev, item_receive_date: date ? date.format("YYYY-MM-DD") : null }))}
-                />
-              </Form.Item>
+              <CustomDatePicker
+                label="現品受領日"
+                value={dates.item_receive_date}
+                onChange={(value) => setDates(prev => ({ ...prev, item_receive_date: value }))}
+              />
             </Col>
             <Col span={6}>
-              <Form.Item label="現品返却日">
-                <DatePicker
-                  value={dates.item_return_date ? dayjs(dates.item_return_date) : undefined}
-                  style={{ width: '100%' }}
-                  onChange={(date) => setDates(prev => ({ ...prev, item_return_date: date ? date.format("YYYY-MM-DD") : null }))}
-                />
-              </Form.Item>
+              <CustomDatePicker
+                label="現品返却日"
+                value={dates.item_return_date}
+                onChange={(value) => setDates(prev => ({ ...prev, item_return_date: value }))}
+              />
             </Col>
             <Col span={6}>
-              <Form.Item label="資料送付日">
-                <DatePicker
-                  value={dates.send_document_date ? dayjs(dates.send_document_date) : undefined}
-                  style={{ width: '100%' }}
-                  onChange={(date) => setDates(prev => ({ ...prev, send_document_date: date ? date.format("YYYY-MM-DD") : null }))}
-                />
-              </Form.Item>
+              <CustomDatePicker
+                label="資料送付日"
+                value={dates.send_document_date}
+                onChange={(value) => setDates(prev => ({ ...prev, send_document_date: value }))}
+              />
             </Col>
             <Col span={6}>
-              <Form.Item label="資料受領日">
-                <DatePicker
-                  value={dates.receive_document_date ? dayjs(dates.receive_document_date) : undefined}
-                  style={{ width: '100%' }}
-                  onChange={(date) => setDates(prev => ({ ...prev, receive_document_date: date ? date.format("YYYY-MM-DD") : null }))}
-                />
-              </Form.Item>
+              <CustomDatePicker
+                label="資料受領日"
+                value={dates.receive_document_date}
+                onChange={(value) => setDates(prev => ({ ...prev, receive_document_date: value }))}
+              />
             </Col>
           </Row>
           <Divider />
@@ -267,13 +237,11 @@ useEffect(() => {
 
           <Row gutter={16}>
             <Col span={12}>
-              <Form.Item label="検収日">
-                <DatePicker
-                  value={dates.accept_date ? dayjs(dates.accept_date) : undefined}
-                  style={{ width: '100%' }}
-                  onChange={(date) => setDates(prev => ({ ...prev, accept_date: date ? date.format("YYYY-MM-DD") : null }))}
-                />
-              </Form.Item>
+              <CustomDatePicker
+                label="検収日"
+                value={dates.accept_date}
+                onChange={(value) => setDates(prev => ({ ...prev, accept_date: value }))}
+              />
             </Col>
             <Col span={12}>
               <Form.Item label="金額" name="amount">
