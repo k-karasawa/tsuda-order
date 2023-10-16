eb110c8eba7ab3c84b77a5599474a2cfc197380c
Your Name <you@example.com>
2023-10-17 08:59:14 +0900

    build

--- src/features/Dashboard/FilterCard.tsx
+++ src/features/Dashboard/FilterCard.tsx
@@ -26,20 +26,18 @@ export const FilterCard: React.FC = () => {
           <Select
             defaultValue="全て"
             style={{ width: 120 }}
-            onChange={handleChange}
             options={[
-              { value: '見積提出', label: '見積提出' },
-              { value: '受注済み', label: '受注済み' },
-              { value: '検証中', label: '検証中' },
-              { value: '失注', label: '失注' },
-              { value: '完了', label: '完了' },
+                { value: '見積提出', label: '見積提出' },
+                { value: '受注済み', label: '受注済み' },
+                { value: '検証中', label: '検証中' },
+                { value: '失注', label: '失注' },
+                { value: '完了', label: '完了' },
             ]}
           />
           依頼内容：
           <Select
             defaultValue="全体"
             style={{ width: 120 }}
-            onChange={handleChange}
             options={[
               { value: '修理', label: '修理' },
               { value: '複製', label: 'OH' },
