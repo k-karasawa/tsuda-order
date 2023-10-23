667e7bd3e0c7a213602c320e946a9a7f22a0762b
Your Name <you@example.com>
2023-10-23 18:11:41 +0900

    export-columns

---
+++ src/features/OrderAssorting/AssortingColumns.tsx
@@ -0,0 +1,37 @@
+import { columns } from '@/data/columns';
+import { Typography } from 'antd';
+
+const ellipsisRender = (text: string) => (
+  <Typography.Text ellipsis={{ tooltip: true }}>{text}</Typography.Text>
+);
+
+export const customColumnsForAssorting = [
+  columns[0], // チェックボックス
+  columns[2], // 受注番号
+  columns[3], // 優先度
+  columns[4], // 進捗
+  columns[5], // 依頼内容
+  columns[6], // 顧客
+  columns[7], // 部署
+  columns[8], // 拠点
+  {
+    ...columns[12], // 品番
+    render: ellipsisRender
+  },
+  {
+    ...columns[13], // 品名
+    render: ellipsisRender
+  },
+  columns[16], // 台数
+  columns[19], // 希望納期
+  columns[20], // 出荷日
+  columns[21], // 現品受領日
+  columns[22], // 現品返却日
+  columns[24], // 資料受領日
+  {
+    ...columns[30], // 備考
+    render: ellipsisRender
+  },
+];
+
+
--- src/features/OrderAssorting/OrderAssorting.tsx
+++ src/features/OrderAssorting/OrderAssorting.tsx
@@ -1,34 +1,13 @@
 import React, { useEffect } from "react";
 import { OrderListContainer } from "@/containers/OrderListContainer";
 import styles from './styles.module.css';
-import { columns } from '@/data/columns';
 import { useRecoilState } from 'recoil';
 import { XScrollState } from '@/recoil/atoms';
-
+import { customColumnsForAssorting } from './AssortingColumns';
 
 type OrderAssortingProps = {
   filterCondition: string;
 };
-
-const customColumnsForAssorting = [
-  columns[0], //チェックボックス
-  columns[2], //受注番号
-  columns[3], //優先度
-  columns[4], //進捗
-  columns[5], //依頼内容
-  columns[6], //顧客
-  columns[7], //部署
-  columns[8], //拠点
-  columns[12], //品番
-  columns[13], //品名
-  columns[16], //台数
-  columns[19], //希望納期
-  columns[20], //出荷日
-  columns[21], //現品受領日
-  columns[22], //現品返却日
-  columns[24], //資料受領日
-  columns[30], //備考
-];
 
 export const OrderAssorting: React.FC<OrderAssortingProps> = ({ filterCondition }) => {
   const [scrollX, setScrollX] = useRecoilState(XScrollState);
