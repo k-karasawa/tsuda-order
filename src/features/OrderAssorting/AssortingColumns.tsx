5847160c612763389e696306e8832531b1cda525
Your Name <you@example.com>
2023-10-24 15:35:53 +0900

    list-modify

--- src/features/OrderAssorting/AssortingColumns.tsx
+++ src/features/OrderAssorting/AssortingColumns.tsx
@@ -10,7 +10,11 @@ export const customColumnsForAssorting = [
   columns[2], // 受注番号
   columns[3], // 優先度
   columns[4], // 進捗
-  columns[5], // 依頼内容
+  columns[19], // 希望納期
+  {
+    ...columns[30], // 備考
+    render: ellipsisRender
+  },
   columns[6], // 顧客
   columns[7], // 部署
   columns[8], // 拠点
@@ -23,15 +27,10 @@ export const customColumnsForAssorting = [
     render: ellipsisRender
   },
   columns[16], // 台数
-  columns[19], // 希望納期
   columns[20], // 出荷日
   columns[21], // 現品受領日
   columns[22], // 現品返却日
   columns[24], // 資料受領日
-  {
-    ...columns[30], // 備考
-    render: ellipsisRender
-  },
 ];
 
 
