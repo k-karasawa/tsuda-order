2f75b554323a36eca85acd241da2ae6b323d6f5f
Your Name <you@example.com>
2023-10-30 14:49:51 +0900

    add-return-form

--- src/features/OrderEditDrawer/SecondaryDrawer.tsx
+++ src/features/OrderEditDrawer/SecondaryDrawer.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
-import { Drawer } from 'antd';
+import { Drawer, Form, Input, Row, Col, Button, DatePicker } from 'antd';
+import { CheckOutlined } from '@ant-design/icons';
+import { FloatButton } from 'antd';
+import moment from 'moment';
 
 interface SecondaryDrawerProps {
   visible: boolean;
@@ -7,16 +10,45 @@ interface SecondaryDrawerProps {
 }
 
 export const SecondaryDrawer: React.FC<SecondaryDrawerProps> = ({ visible, onClose }) => {
+  const [form] = Form.useForm();
+
   return (
     <Drawer
-      title="出戻登録の詳細"
+      title={
+        <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+          出戻登録の詳細
+          <Button onClick={onClose}>
+            キャンセル
+          </Button>
+        </div>
+      }
       width={600}
       onClose={onClose}
       visible={visible}
       destroyOnClose
     >
-      {/* ここにドロワーの内容を追加します */}
-      <div>ドロワーの内容</div>
+      <Form layout="vertical" form={form}>
+        <Row gutter={16}>
+          <Col span={12}>
+            <Form.Item label="出戻日" name="return_date" initialValue={moment()}>
+              <DatePicker style={{ width: '100%' }} format="YYYY-MM-DD" />
+            </Form.Item>
+          </Col>
+          <Col span={12}>
+            <Form.Item label="再出荷日" name="reshipment_date">
+              <DatePicker style={{ width: '100%' }} format="YYYY-MM-DD" />
+            </Form.Item>
+          </Col>
+        </Row>
+        <Row gutter={16}>
+          <Col span={24}>
+            <Form.Item label="備考" name="comment">
+              <Input.TextArea rows={5} placeholder="出戻りの経緯や理由を記入"/>
+            </Form.Item>
+          </Col>
+        </Row>
+      </Form>
+      <FloatButton icon={<CheckOutlined />} type="primary" style={{ right: 24 }} />
     </Drawer>
   );
 };
