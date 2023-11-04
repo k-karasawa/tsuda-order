1aa2851ace86e2ddca7e5b9d0eccd3cb7e146cdb
Your Name <you@example.com>
2023-11-04 17:40:24 +0900

    chart-data-pie-fix

--- src/features/Dashboard/PieCard.tsx
+++ src/features/Dashboard/PieCard.tsx
@@ -1,79 +1,13 @@
 import React from 'react';
 import { PieChart, Pie, Cell, Tooltip, ResponsiveContainer, Legend } from "recharts";
+import { generateSalesData } from './helpers/generateSalesData';
+import { renderCustomizedLabel } from './helpers/renderCustomizedLabel';
 
 export const PieCard: React.FC<{ orderData: any[] }> = ({ orderData }) => {
-  const RADIAN = Math.PI / 180;
   const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042", "#A8368D", "#D15F80"];
 
-  const generateSalesData = () => {
-    const salesData = {
-      受付: 0,
-      検証中: 0,
-      完了: 0,
-      出戻り: 0,
-      失注: 0,
-      作業中: 0,
-    };
-
-    orderData.forEach((data) => {
-      switch (data.progress_name) {
-        case '受付':
-        case '見積提出':
-        case '仮見積':
-          salesData.受付++;
-          break;
-        case '検証中':
-          salesData.検証中++;
-          break;
-        case '完了':
-          salesData.完了++;
-          break;
-        case '出戻り':
-          salesData.出戻り++;
-          break;
-        case '失注':
-          salesData.失注++;
-          break;
-        default:
-          salesData.作業中++;
-          break;
-      }
-    });
-
-    return Object.entries(salesData).map(([name, value]) => ({
-      name,
-      value,
-    }));
-  };
-
-  const SalesData = generateSalesData();
+  const SalesData = generateSalesData(orderData);
   const total = SalesData.reduce((acc, curr) => acc + curr.value, 0);
-
-  const renderCustomizedLabel = ({
-    cx,
-    cy,
-    midAngle,
-    innerRadius,
-    outerRadius,
-    percent
-  }: any) => {
-    const radius = innerRadius + (outerRadius - innerRadius) * 0.6;
-    const x = cx + radius * Math.cos(-midAngle * RADIAN);
-    const y = cy + radius * Math.sin(-midAngle * RADIAN);
-
-    return (
-      <text
-        x={x}
-        y={y}
-        fill="8884d8"
-        textAnchor={x > cx ? "start" : "end"}
-        dominantBaseline="central"
-        fontSize="14"
-      >
-        {`${(percent * 100).toFixed(0)}%`}
-      </text>
-    );
-  };
 
   return (
     <div style={{
@@ -85,7 +19,7 @@ export const PieCard: React.FC<{ orderData: any[] }> = ({ orderData }) => {
       textAlign: 'center'
     }}>
       <ResponsiveContainer width="100%" height={300}>
-      <PieChart margin={{ top: -30, right: 0, bottom: 0, left: 0 }}>
+        <PieChart margin={{ top: -30, right: 0, bottom: 0, left: 0 }}>
           <Pie
             dataKey="value"
             isAnimationActive={false}
@@ -105,7 +39,7 @@ export const PieCard: React.FC<{ orderData: any[] }> = ({ orderData }) => {
             }
           </Pie>
           <Tooltip />
-          <Legend align="center" verticalAlign="bottom" layout="horizontal" wrapperStyle={{ paddingBottom: '20px' }}/>
+          <Legend align="center" verticalAlign="bottom" layout="horizontal" wrapperStyle={{ paddingBottom: '20px' }} />
         </PieChart>
       </ResponsiveContainer>
     </div>
