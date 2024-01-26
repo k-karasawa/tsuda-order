0348d26b65c8cec4863d28a985ccfb70e9429a7c
Your Name <you@example.com>
2024-01-26 19:58:31 +0900

    cronjob

---
+++ utils/chatwork.ts
@@ -0,0 +1,27 @@
+import fetch from 'node-fetch';
+
+const chatworkToken = process.env.CHATWORK_API_TOKEN;
+const chatworkRoomId = process.env.CHATWORK_ROOM_ID;
+
+async function sendChatworkMessage(message: string) {
+  const endpoint = `https://api.chatwork.com/v2/rooms/${chatworkRoomId}/messages`;
+
+  const headers: HeadersInit = {
+    'X-ChatWorkToken': chatworkToken!,
+    'Content-Type': 'application/x-www-form-urlencoded',
+  };
+
+  const response = await fetch(endpoint, {
+    method: 'POST',
+    headers: headers,
+    body: `body=${encodeURIComponent(message)}`,
+  });
+
+  if (!response.ok) {
+    throw new Error(`Chatwork message send failed: ${response.status}`);
+  }
+
+  return response.json();
+}
+
+export { sendChatworkMessage };
