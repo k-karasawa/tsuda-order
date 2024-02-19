b46d788c9602aa339bc1667f2d5e1947a0db9ac1
Your Name <you@example.com>
2024-02-19 18:22:47 +0900

    add-authpage

--- src/pages/auth.tsx
+++ src/pages/auth.tsx
@@ -1,9 +1,23 @@
 import { useSupabaseClient } from '@/hooks/useSupabaseClient';
 import { Auth } from '@supabase/auth-ui-react';
+import { ThemeSupa } from '@supabase/auth-ui-shared';
+import styles from '@/styles/pages/auth.module.scss';
 
 const AuthPage = () => {
   const supabaseClient = useSupabaseClient();
-  return <Auth supabaseClient={supabaseClient} />;
-}
+  return (
+    <Auth
+      supabaseClient={supabaseClient}
+      appearance={{
+        theme: ThemeSupa,
+        className: {
+          container: styles['loginpage-container']
+        },
+        style: {}
+      }}
+      providers={[]}
+    />
+  );
+};
 
 export default AuthPage;
